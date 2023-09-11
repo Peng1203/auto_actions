4187f8bf6968fb0ffa0c85e8b0405be3a0d4cadf
Peng <zhipeng.ma@pbn.com.cn>
2023-09-11 10:56:30 +0800

    updata: auto action 2023-09-11 10:56:30

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ var job = new CronJob(
   function () {
     const nowDate = dayjs().format('YYYY-MM-DD HH:mm:ss');
     fs.appendFile(txtFile, `${nowDate}\t`, (err, data) => {
-      console.log('err ------', err);
-      console.log('data ------', data);
+      if (err) return console.log('写入文件失败 ------', err);
 
       autoActionCommands(nowDate).forEach((command) => {
         exec(command, (err, stdout, stderr) => {
