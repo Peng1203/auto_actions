245e32d68575d899b387b10ff43f4e54198cd264
Peng <zhipeng.ma@pbn.com.cn>
2024-03-01 09:40:28 +0800

    chore(Config): :hammer: 修改 cz 配置文件

--- .cz-config.js
+++ .cz-config.js
@@ -46,28 +46,28 @@ module.exports = {
     }
   ],
 
-  usePreparedCommit: false, // to re-use commit from ./.git/COMMIT_EDITMSG
-  allowTicketNumber: false,
-  isTicketNumberRequired: false,
-  ticketNumberPrefix: 'TICKET-',
-  ticketNumberRegExp: '\\d{1,5}',
+  // usePreparedCommit: false, // to re-use commit from ./.git/COMMIT_EDITMSG
+  // allowTicketNumber: false,
+  // isTicketNumberRequired: false,
+  // ticketNumberPrefix: 'TICKET-',
+  // ticketNumberRegExp: '\\d{1,5}',
 
   // 覆盖消息，默认如下
   messages: {
     type: '请选择提交类型 (必填)',
-    scope: '请选择文件修改范围 (可选)',
+    customScope: '请输入修改范围 (可选)',
+    // 如果 allowCustomScopes 为true则使用 (官方存在bug, 暂未修复)
+    scope: '请选择修改范围 (可选)',
 
-    // 如果allowCustomScopes为true则使用 (官方存在bug, 暂未修复)
-    customScope: '请输入文件修改范围 (可选)',
     subject: '请简要描述提交 (必填)',
     body: '请输入详细描述 (可选)',
-    breaking: '列出任何重大变更 (可选)',
     footer: '请输入要关闭的issue (可选)',
+    // breaking: '列出任何重大变更 (可选)',
     confirmCommit: '确定提交此说明吗？'
   },
 
   allowCustomScopes: true,
-  allowBreakingChanges: ['feat', 'fix'],
-  skipQuestions: ['scope', 'body', 'footer'], // 跳过您想要的任何问题
+  // allowBreakingChanges: ['feat', 'fix'],
+  skipQuestions: ['body', 'footer'], // 跳过您想要的任何问题
   subjectLimit: 72 // 限制主题长度
 }
